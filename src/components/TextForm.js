1a544df50d7179db60e285247e0e0199e4c3ab78
aishwarya1999-k <kalalaishwarya1999@gmail.com>
2024-02-16 10:37:32 +0530

    Make buttons interactive

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,22 +7,40 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText)
     }
+    const handleLowClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+    const handleClearClick = () => {
+        let newText = '';
+        setText(newText)
+    }
     const handleOnChange = (event) => {
         //console.log("clicked")
         setText(event.target.value)
     }
-    const [text, setText] = useState('Enter text here');
+    const [text, setText] = useState('');
     //text = "new text"; //wrong way to change the state
     //setText("new text"); // correct way to change the state
     
   return (
-    <div>
+    <>
+    <div className='container'>
         <h1>{props.heading}</h1>
         <div className="mb-3">
         <textarea className="form-control" id="myBox" rows="8" value={text}  onChange={handleOnChange}></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
-        
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to Lowercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
     </div>
+    <div className="container my-3">
+        <h2>Your Text Summary</h2>
+        <p>{text.split(" ").length} words, {text.length} charaters</p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h3>Preview</h3>
+        <p>{text}</p>
+    </div>
+    </>
   )
 }
